1262f7b646a9692530f5a789c3ce6c7fdecb0db5
Arturo Escudero <darkturo@gmail.com>
2022-11-09 23:18:11 +0100

    read the shabang to detect scripting file types

--- internal/pkg/filetype/filetypes.go
+++ internal/pkg/filetype/filetypes.go
@@ -3,7 +3,11 @@ package filetype
 type FileType string
 
 const (
-	Go FileType = "go"
+	Go     FileType = "go"
+	Python FileType = "python"
+	Perl   FileType = "perl"
+	Ruby   FileType = "ruby"
+	Bash   FileType = "bash"
 
 	// Other is a file type that is not supported by slocc
 	Other FileType = "other"
--- internal/pkg/filetype/magic.go
+++ internal/pkg/filetype/magic.go
@@ -1,9 +1,18 @@
 package filetype
 
-import "path/filepath"
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+)
 
 var extensions = map[string]FileType{
-	".go": Go,
+	".go":   Go,
+	".py":   Python,
+	".pl":   Perl,
+	".rb":   Ruby,
+	".sh":   Bash,
+	".bash": Bash,
 }
 
 // Guess returns the file type of the given file path
@@ -11,5 +20,29 @@ func Guess(path string) FileType {
 	if fileType, ok := extensions[filepath.Ext(path)]; ok {
 		return fileType
 	}
+
+	openFile, err := os.Open(path)
+	if err != nil {
+		return Detect(bufio.NewReader(openFile))
+	}
+	return Other
+}
+
+// Detect detects the FileType of a given input by reading the first line
+func Detect(input *bufio.Reader) FileType {
+	line, _, err := input.ReadLine()
+	if err != nil {
+		switch line {
+		case []byte("#!/usr/bin/env python"), []byte("#!/usr/bin/python"), []byte("#!/usr/bin/python3"):
+			return Python
+		case []byte("#!/usr/bin/env perl"), []byte("#!/usr/bin/perl"):
+			return Perl
+		case []byte("#!/usr/bin/env ruby"), []byte("#!/usr/bin/ruby"):
+			return Ruby
+		case []byte("#!/usr/bin/bash"), []byte("#!/usr/bash"):
+			return Bash
+		}
+	}
+
 	return Other
 }
